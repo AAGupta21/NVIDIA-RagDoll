7baffe081cbd5922680f4cd24fd3ed33f3ad4fb0
AAGupta21 <42502132+AAGupta21@users.noreply.github.com>
2019-08-28 03:42:12 +0530

    Complete Project

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static float Dummy_Health = 25f;
+
+    [SerializeField] private GameObject Dummy = null;
+    [SerializeField] private GameObject Text1 = null;
+    [SerializeField] private GameObject Text2 = null;
+
+    private bool HealthCheckNeeded = true;
+
+    private void Update()
+    {
+        if (HealthCheckNeeded)
+        {
+            this.gameObject.GetComponent<TextMesh>().text = "Health : " + Dummy_Health.ToString();
+            
+            if (Dummy_Health <= 0)
+                LooseScene();
+
+            if (Dummy.transform.position.z >= 10.25f)
+                WinScene();
+        }
+    }
+
+    private void LooseScene()
+    {
+        HealthCheckNeeded = false;
+        Text1.GetComponent<TextMesh>().text = Text2.GetComponent<TextMesh>().text = this.gameObject.GetComponent<TextMesh>().text = "YOU LOOSE!!";
+    }
+
+    private void WinScene()
+    {
+        HealthCheckNeeded = false;
+        Text1.GetComponent<TextMesh>().text = Text2.GetComponent<TextMesh>().text = this.gameObject.GetComponent<TextMesh>().text = "YOU WIN!!";
+    }
+}
